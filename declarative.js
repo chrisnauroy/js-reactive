813de4b69f563236fb2f15c8c02fac0d0902cde6
Christophe Nauroy <151126799+chrisnauroy@users.noreply.github.com>
2026-04-17 16:06:38 +0200

    Implement state management and render function
    Add a simple state management and rendering mechanism.

--- declarative.js
+++ declarative.js
@@ -1,1 +1,18 @@
+let state = {
+  value: 0
+};
+
+function render(state) {
+  document.getElementById("app").innerHTML = `
+    <span>${state.value}</span>
+  `;
+}
+
+document.getElementById("inc").addEventListener("click", () => {
+  state.value++;
+  render(state);
+});
+
+// rendu initial
+render(state);
 
